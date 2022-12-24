a5c42521ae4689b632eb76e718e13ff3ad979b54
Ghost7798 <119027609+Ghost7798@users.noreply.github.com>
2022-12-24 14:30:59 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -3354,12 +3354,20 @@ please rate me
 }
 
 break
-case 'rate': case 'rate2':
+case 'rate5': case 'rate2':
     
     reply(`${pushname}thanks:\n\n for rating baby`)
     break
 
+case 'rate':
+if (isBan) return reply(mess.ban)	 			
+if (isBanChat) return reply(mess.banChat)
+				if (!text) return replay(`Use Text, Example : ${prefix + command} My Dp`)
+					const ra = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100']
+					const te = ra[Math.floor(Math.random() * ra.length)]
+A17.sendMessage(from, { text: `Rate : ${q}\nAnswer : *${te}%*` }, { quoted: m })
 
+					break
 
      case 'join': {
         if (isBan) return reply(mess.banned)	 			
